720ac3db0ad984648d3100d059b563c10820a509
TheHsoa <alexrassk@gmail.com>
2017-02-04 21:07:30 +0700

    SerializeFractionCollection

---
+++ SerializeFractionCollection/SerializeFractionCollection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeFractionCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
